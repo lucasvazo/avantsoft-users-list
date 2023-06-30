771e3a2d60b779351bc4e5155c00b677c92211e0
Lucas Vaz <lucasvazo@hotmail.com>
2023-06-30 14:04:58 -0300

    feat(PagesController): create users table pagination

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PagesController extends Controller
 {
-    public function index() {
-        return view('pages.index');
+    public function index(Request $request)
+    {
+        $response = Http::get('https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0');
+        $data = $response->json()['users'];
+        $currentPage = $request->input('page', 1);
+        $perPage = 10;
+        $totalItems = count($data);
+        $totalPages = ceil($totalItems / $perPage);
+        $currentPageItems = array_slice($data, ($currentPage - 1) * $perPage, $perPage);
+
+        return view('pages.index', [
+            'users' => $currentPageItems,
+            'currentPage' => $currentPage,
+            'totalPages' => $totalPages,
+        ]);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,13 +10,26 @@
             </tr>
         </thead>
         <tbody>
-            {{-- @foreach ($data as $item)
+            @foreach ($users as $user)
                 <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->email }}</td>
+                    <td>{{ $user['id'] }}</td>
+                    <td>{{ $user['name'] }}</td>
+                    <td>{{ $user['email'] }}</td>
                 </tr>
-            @endforeach --}}
+            @endforeach
         </tbody>
     </table>
+    <div class="pag-links">
+        @if ($currentPage > 1)
+            <a href="{{ url()->current() }}?page={{ $currentPage - 1 }}" class="prev">Previous</a>
+        @endif
+
+        @for ($page = 1; $page <= $totalPages; $page++)
+            <a href="{{ url()->current() }}?page={{ $page }}" class="{{ $page == $currentPage ? 'active' : '' }}">{{ $page }}</a>
+        @endfor
+
+        @if ($currentPage < $totalPages)
+            <a href="{{ url()->current() }}?page={{ $currentPage + 1 }}" class="next">Next</a>
+        @endif
+    </div>
 @endsection
