84edade839f933e9459e1e60c3816dfdd1ac9edb
Lucas Vaz <lucasvazo@hotmail.com>
2023-06-30 12:20:55 -0300

    chore(UsersTable): create basic table element

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Table Page</h1>
+    <table class="table-users">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($data as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->email }}</td>
+                </tr>
+            @endforeach --}}
+        </tbody>
+    </table>
 @endsection
