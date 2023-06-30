dd3213abae7466e7e7239ded50c0cde29e91ebaf
Lucas Vaz <lucasvazo@hotmail.com>
2023-06-30 11:37:40 -0300

    style(app.css): add global styling rules

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,5 @@
+<header class="header-main">
+    <div>
+        <img class="header-main-logo" src="https://avantsoft.com.br/images/avantsoft-logo-dark-06d2c1fc.png" >
+    </div>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,11 @@
         <meta name="description" content="AvantSoft Technical Skill Evaluation">
         <meta name="keywords" content="PHP, Laravel, AvantSoft">
         <meta name="author" content="Lucas Vaz">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>AvantSoft</title>
     </head>
     <body class="antialiased">
+        @include('inc.header')
         @yield('content')
     </body>
 </html>
